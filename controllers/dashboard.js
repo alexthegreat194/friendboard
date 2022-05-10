b275a5c9094273caa8f80145e1104210feb64de6
alexthegreat194 <alexander.gentry@aol.com>
2022-05-09 20:31:05 -0700

    can leave group

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -7,21 +7,31 @@ const router = express.Router()
 
 const prisma = require('../utils/prisma')
 
-router.get('/dashboard', async (req, res) => {
-    if (!res.locals.currentUser) {
-        return res.redirect('/login');
-    }
-
-    // see if user has a profile
-    const profile = await prisma.profile.findFirst({
-        where: {
-            userId: res.locals.currentUser.id
+router.use(async (req, res, next) => {
+    if (req.path == '/dashboard') {
+        if (!res.locals.currentUser) {
+            return res.redirect('/login');
         }
-    });
 
-    if (!profile) {
-        return res.redirect('/profile/new')
+        // see if user has a profile
+        const profile = await prisma.profile.findFirst({
+            where: {
+                userId: res.locals.currentUser.id
+            }
+        });
+
+        if (!profile) {
+            return res.redirect('/profile/new')
+        }
+
+        next();
+        
+    } else {
+        next();
     }
+});
+
+router.get('/dashboard', async (req, res) => {
 
     const groups = await prisma.group.findMany({
         include: {
@@ -154,6 +164,46 @@ router.post('/group/join', async (req, res) => {
             groupId: group.id
         }
     });
+
+    res.redirect('/dashboard');
+});
+
+// leave group
+router.get('/group/leave/:code', async (req, res) => {
+    req.params.code = req.params.code.toLowerCase();
+
+    const group = await prisma.group.findFirst({
+        where: {
+            code: req.params.code
+        }
+    });
+
+    if (!group) {
+        console.log('no group');
+        return res.redirect('/dashboard');
+    }
+
+    const userInGroup = await prisma.userInGroup.findFirst({
+        where: {
+            userId: res.locals.currentUser.id,
+            groupId: group.id
+        }
+    });
+
+
+    if (!userInGroup) {
+        console.log('not in group');
+        return res.redirect('/dashboard');
+    }
+
+    await prisma.userInGroup.delete({
+        where: {
+            id: userInGroup.id
+        }
+    });
+
+    console.log(userInGroup);
+    console.log('user left group: ', req.params.code);
 
     res.redirect('/dashboard');
 });
